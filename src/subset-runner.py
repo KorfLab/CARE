8f91ade5d893c1ee5130b87d699c9e0ad1d21190
Henry Li <henrylihaoran@outlook.com>
2025-04-09 13:44:31 -0700

    up

--- src/subset-runner.py
+++ src/subset-runner.py
@@ -25,6 +25,8 @@ parser.add_argument("-g", "--genome", required=True,
 	help="Genome FASTA file")
 parser.add_argument("-y", "--yaml", required=True,
 	help="YAML file of a list of percentages")
+parser.add_argument("-a", "--aligner", nargs='+',
+	help="Aligners to prepare genome folders for")
 
 args = parser.parse_args()
 
@@ -63,3 +65,23 @@ for p in pcts:
 	dst = os.path.join(gdir, f"ref-{int(p * 100)}p.fa")
 	cmd = ["mv", "-f", src, dst]
 	run_cmd(' '.join(cmd))
+
+
+########
+# Move #
+########
+
+if args.aligner:
+	print(f"\n[INFO] Organizing genome subsets for aligners: {args.aligner}")
+
+	refs = [f for f in os.listdir(gdir) if f.startswith("ref-") and f.endswith("p.fa")]
+
+	for aligner in args.aligner:
+		aligner_dir = os.path.join(gdir, f"genomes-{aligner}")
+		os.makedirs(aligner_dir, exist_ok=True)
+
+		for ref in refs:
+			src = os.path.join(gdir, ref)
+			dst = os.path.join(aligner_dir, ref)
+			cmd = ["cp", "-f", src, dst]
+			run_cmd(' '.join(cmd))
